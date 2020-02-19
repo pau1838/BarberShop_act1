b868e6206e1a344fcd5d0d2185139aba5b012839
pau1838 <pau.ibanez@hotmail.com>
2020-02-19 10:19:00 +0100

    First Commit

--- BarberShop/urls.py
+++ BarberShop/urls.py
@@ -1,4 +1,4 @@
-"""BarberShop_act1 URL Configuration
+"""BarberShop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
