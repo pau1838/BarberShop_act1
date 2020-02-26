2107819770c0b682e8d6e99261790f8a8c0bec77
pau1838 <pau.ibanez@hotmail.com>
2020-02-26 09:57:52 +0100

    People models created

--- apps/barberShopApp/models/peopleModels.py
+++ apps/barberShopApp/models/peopleModels.py
@@ -11,14 +11,14 @@ class Barber(models.Model):
         (WHOLE_DAY, 'whole day')
     ]
 
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    turn = models.CharField(max_length=2, choices=TURNS)
-    phone = models.TextField()
+    first_name = models.CharField(name='Nom', max_length=30)
+    last_name = models.CharField(name='Cognoms', max_length=30)
+    turn = models.CharField(name='Torn', max_length=2, choices=TURNS)
+    phone = models.CharField(name='Telèfon', max_length=9)
 
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=50)
-    phone = models.TextField()
+    first_name = models.CharField(name='Nom', max_length=30)
+    last_name = models.CharField(name='Cognoms', max_length=30)
+    email = models.EmailField(name='Email', max_length=50)
+    phone = models.TextField(name='Telèfon')
