2b4cc610c3c4e78fbe265dd4d3edb53611725f2d
ger4rdgg <gerardgg02@gmail.com>
2020-03-05 09:53:27 +0100

    merging branches

--- apps/barberShopApp/urls.py
+++ apps/barberShopApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib import admin
 urlpatterns = [
 
-    path('<int:barberia_id>/', views.detail, name='detail'),
+    path('<int:barberia_name>/', views.detail, name='detail'),
     path('', views.index, name= 'index'),
     path('<int:pk>/', views.barbers_list, name='barbers_list'),
     path('userprofile/', views.test, name='test')
