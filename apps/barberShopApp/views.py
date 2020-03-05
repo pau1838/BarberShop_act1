150de8e5e21cdb3dadb46aa03a443f14c7532af1
ger4rdgg <gerardgg02@gmail.com>
2020-03-05 10:17:58 +0100

    merging branches

--- apps/barberShopApp/views.py
+++ apps/barberShopApp/views.py
@@ -5,8 +5,6 @@ from django.template import loader
 from .models import Barberia
 
 # Create your views here.
-def detail(request, barberia_id):
-    return HttpResponse("Estas buscant la següent barberia %s" % barberia_id)
 
 def index(request):
     llista_barberies = Barberia.objects.order_by('ciutat')
