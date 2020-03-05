3281a3c3c61603360db79b276ad36464c692ff13
pau1838 <pau.ibanez@hotmail.com>
2020-03-05 13:08:10 +0100

    Starting front-end home page

--- BarberShop/settings.py
+++ BarberShop/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../apps/barberShopApp/static/'
