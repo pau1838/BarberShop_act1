07ae06ebeb59a6e671f8f070cef1e5e58db4cbb6
pau1838 <pau.ibanez@hotmail.com>
2020-03-09 23:57:04 +0100

    Responsive design in home page adapted

--- BarberShop/settings.py
+++ BarberShop/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'apps/barberShopApp/static/media')
 )
-MEDIA_URL = '/media/'
+MEDIA_URL = '/apps/barberShopApp/static/media/'
 
