433faaa822eb1a36d488e9feeab5971b726eb902
pau1838 <pau.ibanez@hotmail.com>
2020-03-13 11:29:21 +0100

    Init page barber reservation

--- apps/barberShopApp/models/managementModels.py
+++ apps/barberShopApp/models/managementModels.py
@@ -4,9 +4,9 @@ from .peopleModels import Barber, Client
 
 # Create your models here.
 class Cita(models.Model):
-    barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now=True)
+    barber = models.ForeignKey(Barber, on_delete=models.CASCADE, related_name='appointments')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='appointments')
+    date = models.DateTimeField()
 
 
 class Barberia(models.Model):
